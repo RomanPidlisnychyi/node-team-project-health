6b64a8d189d725824c3438505583aab948f81c12
RomanPidlisnychyi <myLuckyhelper@gmail.com>
2021-02-18 13:02:01 +0200

    init database

---
+++ api/helpers/initDatabase.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const initDatabase = async () => {
+  try {
+    const options = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    };
+
+    await mongoose.connect(process.env.MONGO_URL, options);
+    console.log('Database connection successful');
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+module.exports = initDatabase;
