4ddee9bc541db118884b6c986ded3fb71fdb3a62
Roman <58337316+RomanPidlisnychyi@users.noreply.github.com>
2021-03-13 13:35:14 +0200

    cors

--- api/server.js
+++ api/server.js
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors());
 app.use(morgan('combined'));
 app.use(express.static('public'));
 
